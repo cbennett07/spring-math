68ed1fb2008de827c2c3da944ec46ebd3b8336e5
BENNETT <75508436+cbennett07@users.noreply.github.com>
2021-09-14 12:08:33 -0500

    removed unnecessary example code -Chris B

--- src/main/java/com/example/springplayground/PagesController.java
+++ src/main/java/com/example/springplayground/PagesController.java
@@ -13,22 +13,6 @@ import java.util.Map;
 @RestController
 public class PagesController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
-
-    }
-
-    //example of
-    @GetMapping("/individual-example/{q}/{from}") // matches /individual-example/foo/bar
-    public String getIndividualParams(@PathVariable String from, @PathVariable("q") String query) {
-        return String.format("q:%s from:%s", query, from);
-    }
-
-    @GetMapping("/header")
-    public String getHeader(@RequestHeader String host) {
-        return host;
-    }
 
     @GetMapping("/math/calculate")
     public String getMathString(@RequestParam(required = false, defaultValue = "add") String operation, @RequestParam int x, @RequestParam int y) {
